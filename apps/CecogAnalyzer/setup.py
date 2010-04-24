aafe8d4f67f4ac6c9fabcb69353d4c55ae3289e6
held <held@63e45635-b556-4b11-9482-8189f9e1fe2e>
2010-04-24 16:50:26 +0000

    - fixed a major py2app problem with local Qt installations on MacOS: qt.conf forces Qt libs from app  - one-file .exe for p2exe currently not possible
    git-svn-id: https://svn.cellcognition.org/cecog/trunk@91 63e45635-b556-4b11-9482-8189f9e1fe2e

--- apps/CecogAnalyzer/setup.py
+++ apps/CecogAnalyzer/setup.py
@@ -1,6 +1,6 @@
 """
                            The CellCognition Project
-                     Copyright (c) 2006 - 2009 Michael Held
+                     Copyright (c) 2006 - 2010 Michael Held
                       Gerlich Lab, ETH Zurich, Switzerland
                               www.cellcognition.org
 
@@ -53,7 +53,7 @@ for path in ['dist', 'build']:
         shutil.rmtree(path)
 
 APP = [main_script]
-INCLUDES = ['sip',] # 'netCDF4_utils', 'netcdftime']
+INCLUDES = ['sip', 'netCDF4_utils', 'netcdftime']
 EXCLUDES = ['PyQt4.QtDesigner', 'PyQt4.QtNetwork',
             'PyQt4.QtOpenGL', 'PyQt4.QtScript',
             'PyQt4.QtSql', 'PyQt4.QtTest',
@@ -62,6 +62,7 @@ EXCLUDES = ['PyQt4.QtDesigner', 'PyQt4.QtNetwork',
             'scipy', 'rpy',
             'Tkconstants', 'Tkinter', 'tcl',
             ]
+PACKAGES = ['cecog',]
 
 if sys.platform == 'darwin':
     OPTIONS = {'app' : APP}
@@ -73,7 +74,7 @@ if sys.platform == 'darwin':
                      'dylib_excludes': ['R.framework',],
                      #'frameworks': ['R.framework',],
                      #'strip' : False,
-                     'packages': ['cecog',],
+                     'packages': PACKAGES,
                      'resources': [],
                      'optimize': 2,
                      'compressed': True,
@@ -81,22 +82,24 @@ if sys.platform == 'darwin':
                     }
 elif sys.platform == 'win32':
     import py2exe # pylint: disable-msg=F0401,W0611
-    #FILENAME_ZIP = 'resources/CecogAnalyzer.zip'
-    FILENAME_ZIP = 'CecogAnalyzer.exe'
+    FILENAME_ZIP = 'data.zip'
+    #FILENAME_ZIP = 'CecogAnalyzer.exe'
     OPTIONS = {'windows': [{'script': main_script,
                             'icon_resources': \
                                [(1, r'resources\cecog_analyzer_icon.ico')],
                            }],
-               'zipfile' : None,
+               # FIXME: the one-file version is currently not working!
+               'zipfile' : FILENAME_ZIP,
                }
     SYSTEM = 'py2exe'
     DATA_FILES = []
     EXTRA_OPTIONS = {'includes': INCLUDES,
                      'excludes': EXCLUDES,
-                     'packages': ['cecog',],
+                     'packages': PACKAGES,
                      'optimize': 2,
                      'compressed': True,
                      'bundle_files': 1,
+
                      #'ascii': True,
                      #'xref': True,
                     }
@@ -144,6 +147,8 @@ if sys.platform == 'darwin':
         elif os.path.isfile(filename):
             os.remove(filename)
 
+    # one more special py2app hack forcing Qt to load libs from the app only!
+    shutil.copy('qt.conf', os.path.join(base_path, 'Contents/Resources'))
 
     ## delete ALL .py files from lib (and use .pyc/.pyo instead)
     target = os.path.join(base_path, 'Contents/Resources/lib/python2.6/')
@@ -157,6 +162,11 @@ if sys.platform == 'darwin':
                  'hmm_report.R',
                  'run_hmm.R',
                  ]
+
+    cecog_pyd = os.path.join(target, 'cecog', 'ccore', '_cecog.pyd')
+    if os.path.isfile(cecog_pyd):
+        os.remove(cecog_pyd)
+
     resource_path = os.path.join(base_path, 'Contents/Resources/resources')
     target = os.path.join(resource_path, 'rsrc/hmm')
     safe_mkdirs(target)
@@ -167,19 +177,17 @@ if sys.platform == 'darwin':
 
 
 elif sys.platform == 'win32':
-    import zipfile
+    import zipfile, glob
     lib_filename = os.path.join('dist', FILENAME_ZIP)
     zfile = zipfile.PyZipFile(lib_filename, 'a')
-    vc_path = r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT'
     filenames = [r'C:\Source\Lib\libfftw3-3.dll',
-                 #os.path.join(vc_path, 'msvcm90.dll'),
-                 #os.path.join(vc_path, 'msvcp90.dll'),
-                 #os.path.join(vc_path, 'msvcr90.dll'),
-                 ]
+                 ] +\
+                 glob.glob(r'C:\Source\Microsoft.VC90.CRT\*.*')
     for filename in filenames:
         print "adding '%s' to '%s'" % (filename, lib_filename)
         zfile.write(filename, os.path.split(filename)[1])
     zfile.close()
+
     filenames = ['graph_template.txt',
                  'hmm.R',
                  'hmm_report.R',
