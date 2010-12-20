f6b7b3a3f4fdd8ced7ce90865ba39651ec9bcbc5
held <held@63e45635-b556-4b11-9482-8189f9e1fe2e>
2010-12-20 00:08:13 +0000

    testing now expandable group box widget
    git-svn-id: https://svn.cellcognition.org/cecog/trunk@177 63e45635-b556-4b11-9482-8189f9e1fe2e

---
+++ pysrc/cecog/gui/widgets/__init__.py
@@ -0,0 +1,20 @@
+"""
+                          The CellCognition Project
+                  Copyright (c) 2006 - 2010 Michael Held
+                   Gerlich Lab, ETH Zurich, Switzerland
+                            www.cellcognition.org
+
+           CellCognition is distributed under the LGPL License.
+                     See trunk/LICENSE.txt for details.
+               See trunk/AUTHORS.txt for author contributions.
+"""
+
+__author__ = 'Michael Held'
+__date__ = '$Date$'
+__revision__ = '$Rev$'
+__source__ = '$URL:: $'
+
+
+# this folder contains extensions which do NOT belong to this project,
+# they are NOT written by the authors of this project and are subject to
+# individual licenses.
---
+++ pysrc/cecog/gui/widgets/groupbox.py
@@ -0,0 +1,93 @@
+"""
+                           The CellCognition Project
+                     Copyright (c) 2006 - 2010 Michael Held
+                      Gerlich Lab, ETH Zurich, Switzerland
+                              www.cellcognition.org
+
+              CellCognition is distributed under the LGPL License.
+                        See trunk/LICENSE.txt for details.
+                 See trunk/AUTHORS.txt for author contributions.
+
+Translated to Python and adopted from Qxt (http://www.libqxt.org/)
+"""
+
+__author__ = 'Michael Held'
+__date__ = '$Date$'
+__revision__ = '$Rev$'
+__source__ = '$URL$'
+
+__all__ = ['QxtGroupBox']
+
+#-------------------------------------------------------------------------------
+# standard library imports:
+#
+
+#-------------------------------------------------------------------------------
+# extension module imports:
+#
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.Qt import *
+
+#-------------------------------------------------------------------------------
+# cecog imports:
+#
+
+#-------------------------------------------------------------------------------
+# constants:
+#
+
+#-------------------------------------------------------------------------------
+# functions:
+#
+
+#-------------------------------------------------------------------------------
+# classes:
+#
+
+class QxtGroupBox(QGroupBox):
+
+    def __init__(self, title=None, parent=None):
+        super(QxtGroupBox, self).__init__(parent)
+        if not title is None:
+            self.setTitle(title)
+        self.setCheckable(True)
+        self.setChecked(True)
+        self._collapsive = True
+        self._flat = False
+        self.toggled.connect(self.setExpanded)
+        #self.setStyleSheet('spacing-left: 50px;')
+
+    def isCollapsive(self):
+        return self._collapsive
+
+    def setCollapsive(self, enable=True):
+        if self._collapsive != enable:
+            self._collapsive = enable
+            if not enable:
+                self.setExpanded(True)
+            elif not self.isChecked():
+                self.setExpanded(False)
+
+    @pyqtSlot(bool)
+    def setCollapsed(self, collapsed=True):
+        self.setExpanded(not collapsed)
+
+    @pyqtSlot(bool)
+    def setExpanded(self, expanded=True):
+        if self._collapsive or expanded:
+            for child in self.children():
+                if child.isWidgetType():
+                    child.setVisible(expanded)
+            if expanded:
+                self.setFlat(self._flat)
+            else:
+                self._flat = self.isFlat()
+                self.setFlat(True)
+            self.parent().layout().update()
+
+    def childEvent(self, event):
+        child = event.child()
+        if event.added() and child.isWidgetType():
+            if self._collapsive and not self.isChecked():
+                child.hide()
