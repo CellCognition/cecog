5d747dc0345fe8947b26d55007c231d176d068ee
held <held@63e45635-b556-4b11-9482-8189f9e1fe2e>
2011-01-18 13:31:14 +0000

    added colorized button (QToolButton)
    git-svn-id: https://svn.cellcognition.org/cecog/trunk@197 63e45635-b556-4b11-9482-8189f9e1fe2e

---
+++ pysrc/cecog/gui/widgets/colorbutton.py
@@ -0,0 +1,71 @@
+"""
+                           The CellCognition Project
+                     Copyright (c) 2006 - 2010 Michael Held
+                      Gerlich Lab, ETH Zurich, Switzerland
+                              www.cellcognition.org
+
+              CellCognition is distributed under the LGPL License.
+                        See trunk/LICENSE.txt for details.
+                 See trunk/AUTHORS.txt for author contributions.
+"""
+
+__author__ = 'Michael Held'
+__date__ = '$Date$'
+__revision__ = '$Rev$'
+__source__ = '$URL$'
+
+__all__ = ['ColorButton']
+
+#-------------------------------------------------------------------------------
+# standard library imports:
+#
+
+#-------------------------------------------------------------------------------
+# extension module imports:
+#
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.Qt import *
+
+#-------------------------------------------------------------------------------
+# cecog imports:
+#
+
+#-------------------------------------------------------------------------------
+# constants:
+#
+
+#-------------------------------------------------------------------------------
+# functions:
+#
+
+#-------------------------------------------------------------------------------
+# classes:
+#
+class ColorButton(QToolButton):
+
+    color_changed = pyqtSignal(QColor)
+
+    def __init__(self, current_color, parent, show_alpha=True):
+        QToolButton.__init__(self, parent)
+        self.current_color = current_color
+        self.show_alpha = show_alpha
+        self.clicked.connect(self._on_clicked)
+
+    def set_color(self, color):
+        self.current_color = color
+        self.setStyleSheet('background-color: rgba(%d, %d, %d, %d)'\
+                            % (color.red(), color.green(), color.blue(),
+                               color.alpha()))
+
+    def _on_clicked(self):
+        dlg = QColorDialog(self)
+        if self.show_alpha:
+            dlg.setOption(QColorDialog.ShowAlphaChannel)
+        if not self.current_color is None:
+            dlg.setCurrentColor(self.current_color)
+        if dlg.exec_():
+            col = dlg.currentColor()
+            self.set_color(col)
+            self.color_changed.emit(col)
+
