58ee802f478af3ad73e137b07264d9169b400430
Michael Held <held@cellcognition.org>
2011-08-24 09:34:23 +0200

    bugfix: channel IDs were not assigned correctly after load settings (possible unicode problem)

--- pysrc/cecog/gui/guitraits.py
+++ pysrc/cecog/gui/guitraits.py
@@ -159,7 +159,7 @@ class SelectionTrait2(traits.SelectionTrait2, GuiTrait):
         traits.SelectionTrait2.set_list_data(self, list_data)
         if not self._widget is None:
             current_idx = self._widget.currentIndex()
-            text = self._widget.itemText(current_idx)
+            text = str(self._widget.itemText(current_idx))
             self._widget.clear()
             str_data = map(str, list_data)
             self._widget.addItems(str_data)
