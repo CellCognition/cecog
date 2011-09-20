853ccff092258a78a803ab83779e558e13a4a1f1
walter <twalter@embl.de>
2011-09-20 13:43:20 +0200

    pbs_settings_file for laminB pipeline added.

--- pysrc/scripts/EMBL/cluster/pbs_script_generation.py
+++ pysrc/scripts/EMBL/cluster/pbs_script_generation.py
@@ -154,6 +154,105 @@ cd %s
 
         return
 
+    def exportPBSJobArray_position_based(self, lstExperiments):
+
+        hours = self.oBatchSettings.hours
+        minutes = self.oBatchSettings.minutes
+
+        jobCount = 0
+        jobSize = self.oBatchSettings.jobSize
+        nb_jobs = int(math.ceil(len(lstExperiments)/float(jobSize)))
+
+        # get the list of plates from the tuple list [(plate, pos), (plate, pos), ...]
+        lstPlates = list(set([x[0] for x in lstExperiments]))
+        dctPlateFolder = self.getPlateDirectories(lstPlates)
+
+        # the folders are made at this level in oder to avoid conflicts between parallel jobs.
+        self.makeOutputDirectories(lstPlates, dctPlateFolder)
+
+        # the path command is the first command to be executed in each single job
+        # script. This is practical for setting environment variables,
+        # in particular if there are several version of the same software that are
+        # installed on the cluster.
+        if self.oBatchSettings.path_command is None:
+            path_command = ''
+        else:
+            path_command = self.oBatchSettings.path_command
+
+        # head of each single job script of the array
+        head = """#!/bin/bash
+%s
+cd %s
+""" % (path_command, self.oBatchSettings.batchScriptDirectory)
+
+        additional_options = ''
+        for attribute, value in self.oBatchSettings.additional_attributes.iteritems():
+            additional_options += ' --%s %s' % (attribute, str(value))
+
+        # loop: job array scripts
+        for i in range(nb_jobs):
+            jobCount += 1
+            cmd = ''
+            lstJobPositions = [lstExperiments[k] for k in range(i*jobSize, min(len(lstExperiments), (i+1)*jobSize))]
+
+            for plate, pos in lstJobPositions:
+
+                # command to be executed on the cluster
+                temp_cmd = """
+%s %s -s %s -i %s -o %s -p %s %s"""
+
+                temp_cmd %= (
+                        self.oBatchSettings.pythonBinary,
+                        self.oBatchSettings.batchScript,
+                        self.oBatchSettings.settingsFilename,
+                        self.oBatchSettings.baseInDir,
+                        self.oBatchSettings.baseOutDir,
+                        '___'.join([plate, pos]),
+                        additional_options)
+
+                cmd += temp_cmd
+
+            # this is now written to a script file (simple text file)
+            # the script file is called ltarray<x>.sh, where x is 1, 2, 3, 4, ... and corresponds to the job index.
+            script_name = os.path.join(self.oBatchSettings.baseScriptDir, '%s%i.sh' % (self.oBatchSettings.scriptPrefix, jobCount))
+            script_file = file(script_name, "w")
+            script_file.write(head + cmd)
+            script_file.close()
+
+            # make the script executable (without this, the cluster node cannot call it)
+            os.system('chmod a+x %s' % script_name)
+
+        # write the main script
+        array_script_name = '%s.sh' % os.path.join(self.oBatchSettings.baseScriptDir, self.oBatchSettings.scriptPrefix)
+        main_script_file = file(array_script_name, 'w')
+        main_content = """#!/bin/bash
+#PBS -l walltime=%i:%02i:00
+#PBS -l select=ncpus=%i:mem=%iGb
+#PBS -o %s
+#PBS -e %s
+#PBS -J 1-%i
+#PBS -q %s
+#PBS -M %s
+#PBS -m ae
+%s$PBS_ARRAY_INDEX.sh
+""" % (self.oBatchSettings.hours, self.oBatchSettings.minutes,
+       self.oBatchSettings.ncpus, self.oBatchSettings.mem,
+       self.oBatchSettings.pbsOutDir, self.oBatchSettings.pbsErrDir,
+       jobCount,
+       self.oBatchSettings.clusterName,
+       self.oBatchSettings.pbsMail,
+       os.path.join(self.oBatchSettings.baseScriptDir, self.oBatchSettings.scriptPrefix))
+
+        main_script_file.write(main_content)
+        os.system('chmod a+x %s' % array_script_name)
+
+        # the submission commando is:
+        sub_cmd = '/usr/pbs/bin/qsub -q %s -J1-%i %s' % (self.oBatchSettings.clusterName, jobCount, array_script_name)
+        print sub_cmd
+        print 'array containing %i jobs' % jobCount
+
+        return
+
     def getFinishedJobs(self, i):
         finished_pos = os.listdir(os.path.join(self.oBatchSettings.baseOutDir, plate, 'log', '_finished'))
         return
--- pysrc/scripts/EMBL/settings_files/chromosome_condensation/pbs_laminb_settings.py
+++ pysrc/scripts/EMBL/settings_files/chromosome_condensation/pbs_laminb_settings.py
@@ -6,21 +6,22 @@
 # python-2.7 pbs_script_generation.py -b project_settings/pbs_chromosome_condensation_settings.py
 
 path_command = """export LD_LIBRARY_PATH=/g/software/linux/pack/libboostpython-1.46.1/lib:/g/software/linux/pack/vigra-1.7.1/lib:/g/software/linux/pack/tiff-3.8.1/lib:/g/software/linux/pack/libjpeg-8/lib:/g/software/linux/pack/libpng-1.4.5/lib:/g/software/linux/pack/fftw-3.2.2/lib:/g/software/linux/pack/hdf5-1.8.4/lib:/g/software/linux/pack/szlib-2.1/lib
-export PYTHONPATH=/g/software/linux/pack/cellcognition-1.2.2/SRC/cecog_git/pysrc"""
+export PYTHONPATH=/g/software/linux/pack/cellcognition-1.2.4/SRC/cecog_git/pysrc"""
 
 # data directories
-baseInDir = '/g/ellenberg/JKH/chr_cond_screen'
-baseOutDir = '/g/mitocheck/Thomas/data/JKH/cecog_output'
+baseInDir = '/g/mattaj/Moritz/Olympus data'
+baseOutDir = '/g/mitocheck/Thomas/data/Moritz_analysis_cecog/cecog_output'
 
 # settings for scripts
-baseScriptDir = '/g/mitocheck/Thomas/data/JKH/scripts'
-scriptPrefix = 'mutants'
+baseScriptDir = '/g/mitocheck/Thomas/data/Moritz_analysis_cecog/scripts'
+scriptPrefix = 'LAMINLDA'
 
 # settingsfile
-settingsFilename = '/g/mitocheck/Thomas/data/JKH/cecog_settings/screen_settings_2011-06-09.conf'
+settingsFilename = '/g/mitocheck/Thomas/data/Moritz_analysis_cecog/cecog_settings/settings2011-07-14.conf'
 
 # plates=None means that all plates found in baseInDir are going to be processed.
-plates = None #['plate1_2_006']
+# plates = None #['plate1_2_006']
+plates = ['110820_mutants_LB_Compressed']
 
 batchScriptDirectory = '/g/software/linux/pack/cellcognition-1.2.4/SRC/cecog_git/pysrc/cecog/batch'
 pythonBinary = 'python-2.7'
@@ -28,8 +29,8 @@ batchScript = 'batch.py'
 
 # PBS settings (cluster, walltime, log folders)
 clusterName = 'clng_new'
-pbsOutDir = '/g/mitocheck/PBS/JKH'
-pbsErrDir = '/g/mitocheck/PBS/JKH'
+pbsOutDir = '/g/mitocheck/PBS/laminlda'
+pbsErrDir = '/g/mitocheck/PBS/laminlda'
 pbsMail = 'twalter@embl.de'
 
 hours = 16
@@ -40,7 +41,7 @@ jobSize = 4
 omit_processed_positions = True
 
 additional_attributes = {
-                         'create_images': False,
+                         'create_images': True,
                          }
 
 # folders to be generated
