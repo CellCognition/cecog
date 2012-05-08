e0d25534186902618c229255efb7c646dcd4302a
Christoph Sommer <christoph.sommer@bc.biol.ethz.ch>
2012-05-08 15:04:54 +0200

    HmmThread scafold is working: Please now implement, _readEvents() and _run_plate()

--- pysrc/cecog/gui/analyzer/__init__.py
+++ pysrc/cecog/gui/analyzer/__init__.py
@@ -299,21 +299,74 @@ class _ProcessingThread(QThread):
 
 
 
-class HmmThreadPython(_ProcessingThread):
-    def __init__(self, parent, settings, learner_dict, imagecontainer, oChannel):
+class HmmThread(_ProcessingThread):
+    def __init__(self, parent, settings, learner_dict, imagecontainer):
         _ProcessingThread.__init__(self, parent, settings)
-        # Read out relavent hmm data from given objects
-        pass
+        self._settings.set_section(SECTION_NAME_ERRORCORRECTION)
+        self._learner_dict = learner_dict
+        self._imagecontainer = imagecontainer
+        self.plates = self._imagecontainer.plates
+        self._mapping_files = {}
+        self._logger = logging.getLogger(self.__class__.__name__)
         
-    def _run(self):
-        # do processing
+        # Read Events from event txt files
+        self.events = self._readEvents()
+        
+    def _readEvents(self):
+        "Reads all events written by the CellCognition tracking."
         pass
     
-    def _run_plate(self):
-        # do processing per plate
+    def _setMappingFile(self):
+        if self._settings.get2('position_labels'):
+            path_mapping = self._convert(self._settings.get2('mappingfile_path'))
+            for plate_id in self.plates:
+                mapping_file = os.path.join(path_mapping, '%s.tsv' % plate_id)
+                if not os.path.isfile(mapping_file):
+                    mapping_file = os.path.join(path_mapping, '%s.txt' % plate_id)
+                    if not os.path.isfile(mapping_file):
+                        raise IOError("Mapping file '%s' for plate '%s' not found." %
+                                      (mapping_file, plate_id))
+                self._mapping_files[plate_id] = os.path.abspath(mapping_file)
+        
+    def _run(self):
+        # Initialize GUI Progress bar
+        info = {'min' : 0,
+                'max' : len(self.plates),
+                'stage': 0,
+                'meta': 'Error correction...',
+                'progress': 0}
+        
+        # Process each plate and update Progressbar (if not aborted by user)
+        for idx, plate_id in enumerate(self.plates):
+            if not self._abort:
+                info['text'] = "Plate: '%s' (%d / %d)" % (plate_id, idx+1, len(self.plates))
+                self.set_stage_info(info)
+                self._imagecontainer.set_plate(plate_id)
+                self._run_plate(plate_id)
+                info['progress'] = idx+1
+                self.set_stage_info(info)
+            else:
+                break
+    
+    def _run_plate(self, plate_id):
+        print "processing", plate_id
+    
+    def set_abort(self, wait=False):
         pass
+    
+    @classmethod
+    def test_executable(cls, filename):
+        "mock interface method"
+        return True, ""
+    
+    @classmethod
+    def get_cmd(cls, filename):
+        "mock interface method"
+        return ""
+    
+    
 
-class HmmThread(_ProcessingThread):
+class HmmThread__R_version(_ProcessingThread):
 
     DEFAULT_CMD_MAC = 'R32'
     DEFAULT_CMD_WIN = r'C:\Program Files\R\R-2.10.0\bin\R.exe'
