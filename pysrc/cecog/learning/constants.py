662eeadcaca61103ef5a03ad50bdc52a84ba1a01
Qing Zhong <qing.zhong@bc.biol.ethz.ch>
2012-09-07 15:41:37 +0200

    and another one...

---
+++ pysrc/cecog/learning/constants.py
@@ -0,0 +1,16 @@
+CLASS_COLORS = {0: "#00ff00",
+                1: "#fff643",
+                2: "#ff8000",
+                3: "#d28dce",
+                4: "#8931ea",
+                5: "#5871f1",
+                6: "#FF0000",
+                7: "#94b22d"}
+CLASS_COLORS_LIST=["#00ff00","#fff643","#ff8000","#d28dce","#8931ea","#5871f1","#FF0000","#94b22d"]
+
+#    c1 = [0,1,0]; % green INTER
+#    c2 = [1,246/255,67/255]; % yellow PRO
+#    c3 = [1, 0.5, 0]; % orange PROMETA
+#    c4 = [210/255, 141/255, 206/255]; % pink violet META
+#    c5 = [88/255, 113/255, 241/255]; % blue LATEANA
+#    c6 = [1 0 0]; % red TELO 
