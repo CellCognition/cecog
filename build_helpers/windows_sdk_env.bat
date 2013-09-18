5e6943456ada07773d496901e807b6afb8b34f55
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2013-09-18 17:02:43 +0200

    msvc compiler search path fixed

--- build_helpers/windows_sdk_env.bat
+++ build_helpers/windows_sdk_env.bat
@@ -1,2 +1,7 @@
 :: little helper to set up Windows SDK's build environment
-"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64
+:: certain windows setups do not find the compiler directory
+set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\x86_amd64
+set DISTUTILS_USE_SDK=1
+set MSSdk=1
+"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64 /Release
+
