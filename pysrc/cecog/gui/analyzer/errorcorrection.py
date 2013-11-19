8c8867f992726d6f33dab6a82f298ff89422a402
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2013-11-19 11:08:05 +0100

    option resampling factor added

--- pysrc/cecog/gui/analyzer/errorcorrection.py
+++ pysrc/cecog/gui/analyzer/errorcorrection.py
@@ -74,6 +74,7 @@ class ErrorCorrectionFrame(BaseProcessorFrame):
         self.add_input('compose_galleries')
         self.add_input('multichannel_galleries')
         self.add_input('compose_galleries_sample')
+        self.add_input('resampling_factor')
         self.add_expanding_spacer()
         self._init_control(has_images=False)
 
