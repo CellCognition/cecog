9e2698ccbc555699a83161a9c55f4f7ded9272da
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-03-02 11:13:04 +0100

    Fixing issues #195 and #196

--- cecog/analyzer/position.py
+++ cecog/analyzer/position.py
@@ -482,13 +482,14 @@ class PositionAnalyzer(PositionCore):
             transitions = np.array((0, 1))
         else:
             try:
-                transitions = eval(self.settings.get('EventSelection', 'labeltransitions'))
+                transitions = np.array(eval(self.settings.get('EventSelection', 'labeltransitions')))
+                transitions.reshape((-1, 2))
             except Exception as e:
                 raise RuntimeError(("Make sure that transitions are of the form "
                                     "'int, int' or '(int, int), (int, int)' i.e "
                                     "2-int-tuple  or a list of 2-int-tuples"))
-            transitions = np.array(transitions)
-        return transitions.reshape((-1, 2))
+
+        return transitions
 
     def setup_eventselection(self, graph):
         """Setup the method for event selection."""
@@ -777,7 +778,8 @@ class PositionAnalyzer(PositionCore):
                 if self.settings('Output', 'export_track_data'):
                     self.export_full_tracks()
                 if self.settings('Output', 'export_tracking_as_dot'):
-                    self.export_graphviz()
+                    self.export_graphviz(channel_name =PrimaryChannel.NAME,\
+                                          region_name =self._all_channel_regions[PrimaryChannel.NAME][PrimaryChannel.NAME])
 
             self.export_classlabels()
 
